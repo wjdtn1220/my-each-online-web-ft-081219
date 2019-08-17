3029c11bdf380f2b48e19f335123072b88186072
Jeong Park <wjdtn1220@gmail.com>
2019-08-17 22:01:51 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,8 @@
-def my_each(array)
+def my_each(collection)
   i = 0
-  while i < array.length
-    yield array[i]
+  while i < collection.length
+    yield collection[i]
     i += 1
   end
-  array
+  collection
 end
